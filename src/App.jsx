e93e3de5289cbc07209731b051a3f7fba736e0f7
Dhruv Ohlan <dhruvohlan2112@gmail.com>
2026-03-24 00:17:01 +0530

    Initial batch of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
 import './App.css'
+import { useState, useEffect } from 'react'
+import authService from './appwrite/auth'
+import {useDispatch} from 'react-redux'
+import { login , logout } from './store/authSlice'
+import {Header, Footer} from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  
+  const [loading, setLoading] = useState(true)
+  // every time the app loads, we will check if the user is logged in or not, and then set the loading state to false
+  // It is a good practice to check if the user is logged in or not, before rendering the app, because we don't want to render the app if the user is not logged in, and then redirect them to the login page, because it will cause a flicker effect, and it will also cause a bad user experience.
 
-  return (
-    <>
-      <>
-        <h1>A blog app with appwrite</h1>
-      </>
-    </>
-  )
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if(userData){
+          dispatch(login({userData}))
+        }else{
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+    
+  }, [])
+
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
